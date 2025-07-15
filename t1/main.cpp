8359b013ceaadf2b09029e8fbbe36d039f0db6ab
Achilles0326 <Achilles0326@sjtu.edu.cn>
2025-07-15 22:03:24 +0800

    Initial Commit

---
+++ t1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  t1
+//
+//  Created by 郭天宇 on 2025/7/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
