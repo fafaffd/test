9ab257d9b5966d11ec2332271805129849a4107e
Achilles0326 <Achilles0326@sjtu.edu.cn>
2025-07-15 21:44:36 +0800

    add b.cpp to branch1

---
+++ b.cpp
@@ -0,0 +1,17 @@
+//
+//  b.cpp
+//  
+//
+//  Created by 郭天宇 on 2025/7/15.
+//
+
+#include <string>
+#include <vector>
+
+int main(){
+    vector<int> a(10,1);
+    for(auto i : a){
+        std::cout<<i<<' ';
+    }
+    return 0;
+}
