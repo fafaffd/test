bd78a256aa23c972bc5177353017d58bf7b991ae
Achilles0326 <Achilles0326@sjtu.edu.cn>
2025-07-15 22:38:51 +0800

    add new to branch3

---
+++ t1/include/mylib.h
@@ -0,0 +1,12 @@
+//
+//  mylib.h
+//  t1
+//
+//  Created by 郭天宇 on 2025/7/15.
+//
+
+#pragma once
+
+namespace mylib {
+    int add(int a, int b);
+}
--- t1/src/main.cpp
+++ t1/src/main.cpp
@@ -6,9 +6,10 @@
 //
 
 #include <iostream>
+#include "mylib.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << mylib::add(3, 5);
     return 0;
 }
